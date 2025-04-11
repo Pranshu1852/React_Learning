2107b7d8b31747e56c91ff3080f604b1eea3c151
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-04-11 16:19:51 +0530

    Add: Learn to use React using webpack

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+let flag=true;
+let counter=0;
+
+
+const handleClick=()=>{
+    console.log(++counter);
+}
+
+const trueElement=<div className="trueelement" onClick={handleClick}>This is a true Element</div>;
+const falseElement=<div className="falseelement">This is a false Element</div>;
+const div=<div className="container">{flag?trueElement:falseElement}</div>;
+const CustomComponent=()=>{
+    return <>
+    <div>
+        This is a customComponent.
+    </div>
+    hello
+    </>;
+}
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+// root.render(div);
+root.render(<CustomComponent></CustomComponent>);
