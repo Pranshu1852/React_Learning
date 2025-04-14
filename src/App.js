4e45a0539882cb3f8181a2ae887ca6fa76206016
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-04-14 16:09:05 +0530

    Add: Learn about useState hook in react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 import Button from "./Button";
+import Text from "../Text";
 
 const App=()=>{
     let counter=0;
+    const [data, setData]=useState([{id:'a',text:'text 0'},
+        {id:'b',text:'text 1'},
+        {id:'c',text:'text 2'},
+        {id:'d',text:'text 3'},
+        {id:'e',text:'text 4'},
+    ])
     const handleClick=()=>{
-        console.log(++counter);
+        setData((prevdata)=>[...prevdata,{id:'f',text:'text 5'}])
     }
 
-    return <Button description="This is a button" clickAction={handleClick}>Click</Button>;
+    return <>
+    {data.map((item,index)=>{
+        return <Text key={index}>{item.text}</Text>;
+    })}
+    <Button clickAction={handleClick}>Add new</Button>;
+    </>;
 }
 
 export default App;
